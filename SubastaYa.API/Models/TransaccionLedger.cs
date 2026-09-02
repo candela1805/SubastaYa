e401ce93c66dfbcd481ad2671a61f6e6f73aa547
Candela Luna <121209394+candela1805@users.noreply.github.com>
2026-09-01 23:41:56 -0300

    feat: creacion entidad TransaccionLedger

---
+++ SubastaYa.API/Models/TransaccionLedger.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SubastaYa.API.Models
+{
+    public class TransaccionLedger
+    {
+        [Key]
+        public int Id{ get; set; }
+        public int BilleteraId{ get; set; }
+
+        // Deposito, Retencion, Liberacion, Pago, Cobro
+        public string Tipo { get; set; }
+
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal Monto{ get; set; }
+        public DateTime Fecha{ get; set; }
+
+        public int? SubastaId{ get; set; }
+    }
+}
